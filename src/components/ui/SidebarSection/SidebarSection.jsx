3df1f7f780eadf3a434bb38242148537eab39120
lutfi aulia sidik <biolutfi@gmail.com>
2023-12-15 13:24:22 +0700

    membuat menu sidebar active terhadap page nya

--- src/components/ui/SidebarSection/SidebarSection.jsx
+++ src/components/ui/SidebarSection/SidebarSection.jsx
@@ -1,11 +1,11 @@
 import './SidebarSection.css'
 import * as Icon from 'react-bootstrap-icons'
 import { useNavbarStatus } from '../../../stores/app'
+import { NavLink } from 'react-router-dom'
 
 const SidebarSection = () =>
 {
   const { status } = useNavbarStatus(state => ({ status: state.status }))
-  const getUrlPath = document.baseURI.split('/')[3]
 
   return ( 
     <sidebar className={`container sidebar ${ status ? 'active' : ''}`}>
@@ -16,14 +16,18 @@ const SidebarSection = () =>
       <div className='wrapper sidebar-menu'>
         <h1>main menu</h1>
         <ul>
-          <a href='/'><li>
-            <Icon.GridFill/>
-            <p>dashboard</p>
-          </li></a>
-          <a href='/product'><li>
-            <Icon.BoxSeamFill/>
-            <p>product</p>
-          </li></a>
+          <NavLink to='/'>
+            <li>
+              <Icon.GridFill/>
+              <p>dashboard</p>
+            </li>
+          </NavLink>
+          <NavLink to='/product'>
+            <li>
+              <Icon.BoxSeamFill/>
+              <p>product</p>
+            </li>
+          </NavLink>
         </ul>
       </div>
     </sidebar>
